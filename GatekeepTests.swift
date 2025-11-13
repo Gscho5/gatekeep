4fa985cf24b4cf45a18b0194b20d1d9841c6cbed
Gabe Schoor <152760456+Gscho5@users.noreply.github.com>
2025-11-12 20:45:14 -0800

    Add files via upload

---
+++ GatekeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  GatekeepTests.swift
+//  GatekeepTests
+//
+//  Created by Gabe Schoor on 11/12/25.
+//
+
+import Testing
+@testable import Gatekeep
+
+struct GatekeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
