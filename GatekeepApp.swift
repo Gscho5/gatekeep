6d01b2438694e49c037759deb1ccf2a8733e70b6
Gabe Schoor <152760456+Gscho5@users.noreply.github.com>
2025-11-12 20:26:16 -0800

    Add files via upload

---
+++ GatekeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatekeepApp.swift
+//  Gatekeep
+//
+//  Created by Gabe Schoor on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GatekeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
